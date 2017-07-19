d790f1a5191bcb899cd2c48b9c953f81879346e5
Natalie Villasana <navillasa@gmail.com>
2017-07-19 15:08:00 -0400

    added ball machine constants and dictionary making function

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,17 @@
-function makeNumberString(someRange) {
-   var numberString = ""
-   return numberString.reduce(function (someRange) {
-        return String(Math.floor(Math.random() * someRange) + 1);
-}, 1);
+var BIG_BALL_MACHINE = 69;
+var SMALL_BALL_MACHINE = 26;
+
+var lotteryNums = {};
+
+var num = function getRandom(someRange) {
+    return Math.floor(Math.random() * someRange) + 1;
+};
+
+// is number already in running list? if so, get a different random
+function isThere(lotteryNums, num) {
+    if (lotteryNums.keys().length > 5) {
+
+    }
 }
 
-function fiveNumber(someRange) {
-    return makeNumberString(someRange) * 5;
-}
-
-console.log(fiveNumber(69));
+function makeFiveNumList()
