2e368a26faaf3a54a0ef818b5a8fa0fc08eee109
Natalie Villasana <navillasa@gmail.com>
2017-07-19 15:46:19 -0400

    got working recursive function to create an array of five unique number strings

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,38 @@
-var BIG_BALL_MACHINE = 69;
-var SMALL_BALL_MACHINE = 26;
+// var BIG_BALL_MACHINE = 69;
+// var SMALL_BALL_MACHINE = 26;
 
-var lotteryNums = {};
 
-var num = function getRandom(someRange) {
-    return Math.floor(Math.random() * someRange) + 1;
-};
+// var num = function getRandom(someRange) {
+//     return Math.floor(Math.random() * someRange) + 1;
+// };
 
-// is number already in running list? if so, get a different random
-function isThere(lotteryNums, num) {
-    if (lotteryNums.keys().length > 5) {
+// function numberListMaker(num) {
+//     var lotteryNums = {};
+//     function (lotteryNums) {
 
+//     }
+//         lotteryNums[element] = true;
+//     })
+//     if (lotteryNums.keys().length > size) {
+
+// function makeFiveNumList()
+
+
+function getList(){
+    dict = {};
+    function recursion(dict){
+        if (Object.keys(dict).length == 5) {
+            return Object.keys(dict);
+        } else {
+            dict[(Math.floor(Math.random() * 69) + 1).toString()] = true;
+            recursion(dict);
+        }
+        return Object.keys(dict);
     }
+
+    return recursion(dict);
 }
 
-function makeFiveNumList()
+for(var x = 0; x < 40; x++){
+    console.log(getList())
+}
