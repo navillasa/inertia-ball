5ccc8e10b794f04d1b3b3dd15b70aa994a32f5fd
Natalie Villasana <navillasa@gmail.com>
2017-07-20 10:58:15 -0400

    added jquery link but still working on old school dom manipulation

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ var fiveNumList = function getList(){
         if (Object.keys(dict).length == 5) {
             return Object.keys(dict);
         } else {
-            dict[(Math.floor(Math.random() * 69) + 1).toString()] = true;
+            dict[(Math.floor(Math.random() * 69) + 1).toString())] = true;
             recursion(dict);
         }
         return Object.keys(dict);
@@ -16,9 +16,34 @@ var fiveNumList = function getList(){
 }
 
 function rando(ballMachine) {
-    return (Math.floor(Math.random() * ballMachine) + 1;
+    return (Math.floor(Math.random() * ballMachine)) + 1;
 }
 
 function addInertiaNum(fiveNumList) {
 
 }
+
+// var num;
+
+// while (picks.length < howMany) {
+//     num = randomBetween(highNumber);
+
+
+//     if (picks.indexOf(num) === -1) {
+//         picks.push(num);
+//     }
+// }
+
+// return picks;
+
+// trigger.addEventListener('click', function () {
+//     console.log('hey');
+
+//     return generateLottoNumbers(howMany, maxNum);
+// });
+
+// addEventListener() doesn't return values!
+// so don't call it
+// and don't return from inside of a callback
+// because the value goes nowhere!
+// addEventListener should go in "main" function
